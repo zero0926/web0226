4d864af7aa64107a43d9c083702ca22fd92b8572
zero <nightcore401512@gmail.com>
2024-05-20 16:58:33 +0800

    0520(到14:05

---
+++ WebApplication0226/Models/memeM.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication0226.Models;
+
+public class memeM
+{
+    public string src { get; set; }
+    public string title { get; set; }
+}
---
+++ WebApplication0226/Services/memeService.cs
@@ -0,0 +1,21 @@
+﻿using WebApplication0226.Models;
+
+namespace WebApplication0226.Services;
+
+public class memeService
+{
+    public List<memeM> List()
+    {
+        List<memeM> result = new List<memeM>();
+        try
+        {
+
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
+        return result;
+    }
+}
